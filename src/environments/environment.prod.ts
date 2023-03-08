75a7c9b20e74269702281f8b34c992969a53102d
CarlosBrrs <carlosbarrios148@gmail.com>
2023-03-07 19:48:35 -0500

    Configuracion de web app Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBI4l1ROJxfvmPDxsx9adZKJxNi9tz4fuk",
+    authDomain: "ra-fitness-sport.firebaseapp.com",
+    projectId: "ra-fitness-sport",
+    storageBucket: "ra-fitness-sport.appspot.com",
+    messagingSenderId: "473243346055",
+    appId: "1:473243346055:web:081af92049e5b971c60422"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBI4l1ROJxfvmPDxsx9adZKJxNi9tz4fuk",
+    authDomain: "ra-fitness-sport.firebaseapp.com",
+    projectId: "ra-fitness-sport",
+    storageBucket: "ra-fitness-sport.appspot.com",
+    messagingSenderId: "473243346055",
+    appId: "1:473243346055:web:081af92049e5b971c60422"
+  }
 };
 
 /*
